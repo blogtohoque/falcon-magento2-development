5bea3be143d19e972bc75284dd206176ea98d422
Sergii Gozhedrianov <sergii@deity.io>
2019-06-20 14:57:12 +0200

    added unit test for main service class, plus some config changes

--- app/code/Deity/Cms/Model/GetStaticBlockContent.php
+++ app/code/Deity/Cms/Model/GetStaticBlockContent.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace Deity\Cms\Model;
 
 use Deity\CmsApi\Api\GetStaticBlockContentInterface;
+use Deity\CmsApi\Model\GetBlockByIdentifierInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Widget\Model\Template\FilterEmulate;
 
 /**
  * Class GetStaticBlockContent
@@ -14,13 +18,48 @@ class GetStaticBlockContent implements GetStaticBlockContentInterface
 {
 
     /**
+     * @var GetBlockByIdentifierInterface
+     */
+    private $getBlockByIdentifier;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var FilterEmulate
+     */
+    private $contentFilter;
+
+    /**
+     * GetStaticBlockContent constructor.
+     * @param GetBlockByIdentifierInterface $getBlockByIdentifier
+     * @param StoreManagerInterface $storeManager
+     * @param FilterEmulate $filterEmulate
+     */
+    public function __construct(
+        GetBlockByIdentifierInterface $getBlockByIdentifier,
+        StoreManagerInterface $storeManager,
+        FilterEmulate $filterEmulate
+    ) {
+        $this->contentFilter = $filterEmulate;
+        $this->getBlockByIdentifier = $getBlockByIdentifier;
+        $this->storeManager = $storeManager;
+    }
+
+
+    /**
      * Get content of the static block
      *
      * @param string $identifier
      * @return string
+     * @throws NoSuchEntityException
      */
     public function execute(string $identifier): string
     {
-        // TODO: Implement execute() method.
+        $storeId = (int)$this->storeManager->getStore()->getId();
+        $blockInstance = $this->getBlockByIdentifier->execute($identifier, $storeId);
+        return (string)$this->contentFilter->filter($blockInstance->getContent());
     }
 }
---
+++ app/code/Deity/Cms/Test/Unit/Model/GetStaticBlockContentTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Deity\Cms\Model;
+
+use Magento\Cms\Model\Block;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManager;
+use Magento\Widget\Model\Template\FilterEmulate;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+
+class GetStaticBlockContentTest extends TestCase
+{
+
+    /**
+     * @var GetStaticBlockContent
+     */
+    private $getStaticBlockContent;
+
+    /**
+     * @var GetBlockByIdentifier | MockObject
+     */
+    private $getBlockByIdentifier;
+
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var StoreManager | MockObject
+     */
+    private $storeManager;
+
+    public function setUp()
+    {
+        $this->objectManager = new ObjectManager($this);
+
+        $this->getBlockByIdentifier = $this->createPartialMock(
+            GetBlockByIdentifier::class,
+            ['execute']
+        );
+
+        $storeObject = $this->createPartialMock(
+            Store::class,
+            ['getId']
+        );
+        $storeObject
+            ->expects($this->any())
+            ->method('getId')
+            ->will($this->returnValue(1));
+        $this->storeManager = $this->createPartialMock(
+            StoreManager::class,
+            ['getStore']
+        );
+
+        $this->storeManager->expects($this->any())
+            ->method('getStore')
+            ->will($this->returnValue($storeObject));
+
+        $filterEmulate = $this->createMock(FilterEmulate::class);
+        $filterEmulate->expects($this->any())
+            ->method('filter')
+            ->will($this->returnArgument(0));
+
+        $this->getStaticBlockContent = $this->objectManager->getObject(
+            GetStaticBlockContent::class,
+            [
+                'storeManager' => $this->storeManager,
+                'getBlockByIdentifier' => $this->getBlockByIdentifier,
+                'filterEmulate' => $filterEmulate
+            ]
+        );
+    }
+
+    public function testExecute()
+    {
+        $testIdentifier = 'any-block';
+        $testBlockContent = 'any-content';
+
+        $block = $this->createMock(Block::class);
+
+        $block->expects($this->any())
+            ->method('getContent')
+            ->will($this->returnValue($testBlockContent));
+        $this->getBlockByIdentifier
+            ->expects($this->any())
+            ->method('execute')
+            ->will($this->returnValue($block));
+
+        $blockContent = $this->getStaticBlockContent->execute($testIdentifier);
+        $this->assertEquals(
+            $testBlockContent,
+            $blockContent,
+            'Service should return block content'
+        );
+    }
+}
+
