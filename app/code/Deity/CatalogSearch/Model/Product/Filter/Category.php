c4dca900b434fe3e2af82b78c76327abc66e7d62
Sergii Gozhedrianov <sergii@deity.io>
2020-02-21 11:24:54 +0000

    Merge pull request #140 from sergy-gzh/bugfix/fixing-search-filters
    Bugfix/fixing search filters

---
+++ app/code/Deity/CatalogSearch/Model/Product/Filter/Category.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+namespace Deity\CatalogSearch\Model\Product\Filter;
+
+use Deity\CatalogApi\Api\Data\FilterInterface;
+use Deity\CatalogApi\Api\Data\FilterInterfaceFactory;
+use Deity\CatalogApi\Api\Data\FilterOptionInterface;
+use Deity\CatalogApi\Api\Data\FilterOptionInterfaceFactory;
+use Deity\CatalogApi\Model\Product\FilterDataRendererInterface;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Catalog\Model\Layer;
+use Magento\Catalog\Model\Layer\Filter\AbstractFilter;
+use Magento\Catalog\Model\Layer\Filter\Item;
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * Class Category
+ *
+ * @package Deity\CatalogSearch\Model\Product\Filter
+ */
+class Category implements FilterDataRendererInterface
+{
+    /**
+     * @var FilterInterfaceFactory
+     */
+    private $filterFactory;
+
+    /**
+     * @var FilterOptionInterfaceFactory
+     */
+    private $filterOptionFactory;
+
+    /**
+     * @var CategoryRepositoryInterface
+     */
+    private $categoryRepository;
+
+    /**
+     * Attribute constructor.
+     * @param FilterInterfaceFactory $filterFactory
+     * @param FilterOptionInterfaceFactory $filterOptionFactory
+     * @param CategoryRepositoryInterface $categoryRepository
+     */
+    public function __construct(
+        FilterInterfaceFactory $filterFactory,
+        FilterOptionInterfaceFactory $filterOptionFactory,
+        CategoryRepositoryInterface $categoryRepository
+    ) {
+        $this->categoryRepository = $categoryRepository;
+        $this->filterFactory = $filterFactory;
+        $this->filterOptionFactory = $filterOptionFactory;
+    }
+
+    /**
+     * Get filter data for category
+     *
+     * @param Layer $layer
+     * @param AbstractFilter $magentoFilter
+     * @param array $selectedValues
+     * @return FilterInterface
+     * * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @throws LocalizedException
+     */
+    public function getFilterData(
+        Layer $layer,
+        AbstractFilter $magentoFilter,
+        array $selectedValues = []
+    ) : FilterInterface {
+        /** @var FilterInterface $filterObject */
+        $filterObject = $this->filterFactory->create(
+            [
+                FilterInterface::LABEL => (string)$magentoFilter->getName(),
+                FilterInterface::CODE => $magentoFilter->getRequestVar(),
+                FilterInterface::TYPE => 'int',
+                FilterInterface::ATTRIBUTE_ID => 0
+            ]
+        );
+        $magentoOptions = $magentoFilter->getItems();
+
+        /** @var Item $magentoOption */
+        foreach ($magentoOptions as $magentoOption) {
+            /** @var FilterOptionInterface $filterOption */
+            $filterOption =$this->filterOptionFactory->create(
+                [
+                    FilterOptionInterface::LABEL => (string)$magentoOption->getData('label'),
+                    FilterOptionInterface::VALUE => $magentoOption->getValueString(),
+                    FilterOptionInterface::COUNT => (int)$magentoOption->getData('count'),
+                    FilterOptionInterface::IS_SELECTED => false
+                ]
+            );
+            $filterObject->addOption($filterOption);
+        }
+
+        if (!empty($selectedValues)) {
+            $currentCategory = $this->categoryRepository->get(array_pop($selectedValues));
+            $filterOption =$this->filterOptionFactory->create(
+                [
+                    FilterOptionInterface::LABEL => $currentCategory->getName(),
+                    FilterOptionInterface::VALUE => $currentCategory->getId(),
+                    FilterOptionInterface::COUNT => $layer->getProductCollection()->getSize(),
+                    FilterOptionInterface::IS_SELECTED => true
+                ]
+            );
+            $filterObject->addOption($filterOption);
+        }
+
+        return $filterObject;
+    }
+}
